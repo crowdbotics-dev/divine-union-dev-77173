6bf0aacbcc650155daefd32a398914bc7a6dd4e1
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 00:18:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-union-dev-77173",
+  url: "https://divine-union-dev-77173.botics.co",
+  api: "https://divine-union-dev-77173.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
